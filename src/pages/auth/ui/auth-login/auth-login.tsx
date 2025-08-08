c8a7a4a4c7c06485edb549a9b41b200153aff8e5
Nikita Kosachev <kosachemoto@proton.me>
2025-08-08 13:41:37 +0500

    feat(pages): add login page

--- src/pages/auth/ui/auth-login/auth-login.tsx
+++ src/pages/auth/ui/auth-login/auth-login.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+
+import '@/widgets/auth/ui/auth-code-request-form';
+import { AuthLoginForm } from '@/widgets/auth/ui/auth-login-form';
+
+import { useAuthLoginMutation } from '@/shared/api/auth/login/hooks';
+import { useAlert } from '@/shared/hooks/alert';
+import { Alert } from '@/shared/ui/alert';
+
+import type { TAuthLogin } from '@/entities/auth/types';
 
 export const AuthLogin: React.FC = () => {
-    return <code>login</code>;
+    const { props, setError } = useAlert();
+    const navigate = useNavigate();
+    const authLoginMutation = useAuthLoginMutation();
+
+    const authLogin = (data: TAuthLogin) => {
+        authLoginMutation.mutate(data, {
+            onSuccess: () => navigate('/auth/success'),
+            onError: setError,
+        });
+    };
+
+    return (
+        <>
+            <h1>Login</h1>
+            {authLoginMutation.isError && <Alert {...props} />}
+            <AuthLoginForm
+                onSubmit={authLogin}
+                isLoading={authLoginMutation.isPending}
+            />
+        </>
+    );
 };
