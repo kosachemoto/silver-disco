55c02a4e79758d49928f9e9f025c569a9b0f2305
Nikita Kosachev <kosachemoto@proton.me>
2025-08-08 13:55:19 +0500

    feat(pages): add success page

--- src/pages/auth/ui/auth-success/auth-success.tsx
+++ src/pages/auth/ui/auth-success/auth-success.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+import { Alert } from '@/shared/ui/alert';
+import { Link } from '@/shared/ui/link';
+
 export const AuthSuccess: React.FC = () => {
-    return <code>success</code>;
+    return (
+        <>
+            <h1>Success</h1>
+            <Alert variant="success">You've done it! Congratulations</Alert>
+            <p>Take a look on other flows if you haven't done it yet</p>
+            <Link to="/auth">Get back</Link>
+        </>
+    );
 };
