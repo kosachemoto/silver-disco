59517285daaace0f41e874641e9b77b81c1ad8bb
Nikita Kosachev <kosachemoto@proton.me>
2025-08-08 20:15:11 +0500

    feat(pages): add passkey flow on code request page

--- src/pages/auth/ui/auth-code-request/auth-code-request.tsx
+++ src/pages/auth/ui/auth-code-request/auth-code-request.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
 
+import { useAuthPasskeyButton } from '@/pages/auth/hooks';
 import { useAuthCodeRouteState } from '@/pages/auth/hooks';
 
 import { AuthCodeRequestForm } from '@/widgets/auth/ui/auth-code-request-form';
 
 import { useAuthCodeRequestMutation } from '@/shared/api/auth/code/request/hooks';
+import { useAuthPasskeyMutation } from '@/shared/api/auth/passkey/hooks';
 import { useAlert } from '@/shared/hooks/alert';
 import { Alert } from '@/shared/ui/alert';
+import { Button } from '@/shared/ui/button';
+import { Divider } from '@/shared/ui/divider';
 
 import type { TAuthCodeRequest } from '@/entities/auth/types';
 
 export const AuthCodeRequest: React.FC = () => {
-    const { props, setApiError } = useAlert();
-    const navigate = useNavigate();
     const { email } = useAuthCodeRouteState();
+    const {
+        props: propsButton,
+        onStart,
+        onSuccess,
+        onError,
+    } = useAuthPasskeyButton();
+    const { props: propsAlert, setApiError } = useAlert();
+    const navigate = useNavigate();
     const authCodeRequestMutation = useAuthCodeRequestMutation();
+    const authPasskeyMutation = useAuthPasskeyMutation();
     const authCodeRequest = (data: TAuthCodeRequest) => {
         authCodeRequestMutation.mutate(data, {
             onSuccess: () => {
@@ -27,14 +38,34 @@ export const AuthCodeRequest: React.FC = () => {
         });
     };
 
+    const authPasskey = () => {
+        onStart();
+        authPasskeyMutation.mutate(undefined, {
+            onSuccess: () => {
+                onSuccess();
+                navigate('/auth/success');
+            },
+            onError: (err) => {
+                onError();
+                setApiError(err);
+            },
+        });
+    };
+
     return (
         <>
             <h1>Login</h1>
-            {authCodeRequestMutation.isError && <Alert {...props} />}
+            {authCodeRequestMutation.isError && <Alert {...propsAlert} />}
             <AuthCodeRequestForm
                 onSubmit={authCodeRequest}
                 defaultValues={{ email }}
                 isLoading={authCodeRequestMutation.isPending}
+            />
+            <Divider>or</Divider>
+            <Button
+                variant="secondary"
+                onClick={authPasskey}
+                {...propsButton}
             />
         </>
     );
