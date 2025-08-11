df6761a9f4f69d82003c5bda8bfa226400d918c0
Nikita Kosachev <kosachemoto@proton.me>
2025-08-11 20:00:15 +0500

    chore: apply prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,45 @@
-import { createHash } from 'crypto';
-import { defineConfig } from 'vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { createHash } from 'crypto';
 import fs from 'node:fs';
 import path from 'node:path';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), {
-      name: "generate-404",
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    svgr(),
+    {
+      name: 'generate-404',
       closeBundle() {
-        const distDir = path.resolve(__dirname, "dist");
-        const indexPath = path.join(distDir, "index.html");
-        const notFoundPath = path.join(distDir, "404.html");
+        const distDir = path.resolve(__dirname, 'dist');
+        const indexPath = path.join(distDir, 'index.html');
+        const notFoundPath = path.join(distDir, '404.html');
         if (fs.existsSync(indexPath)) {
           fs.copyFileSync(indexPath, notFoundPath);
         }
       },
-    },],
-    resolve: {
+    },
+  ],
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   css: {
     modules: {
-      generateScopedName: function(element, filename) {
+      generateScopedName: function (element, filename) {
         const block = path.basename(filename, '.module.css');
-        const hash = createHash('sha256').update(filename).digest('hex').slice(0, 5);
+        const hash = createHash('sha256')
+          .update(filename)
+          .digest('hex')
+          .slice(0, 5);
 
         if (element.startsWith('root_')) {
           return `${block}${element.substring(4)}--${hash}`;
@@ -38,8 +50,8 @@ export default defineConfig({
         }
 
         return `${block}--${hash}`;
-      }
-    }
+      },
+    },
   },
-  base: 'silver-disco'
+  base: 'silver-disco',
 });
