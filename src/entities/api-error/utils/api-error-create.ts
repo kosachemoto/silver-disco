b1f82207c5bf1dfaa6e2abc61ebade41926c31e9
Nikita Kosachev <kosachemoto@proton.me>
2025-08-12 19:22:45 +0500

    fix: fix api error handling

--- src/entities/api-error/utils/api-error-create.ts
+++ src/entities/api-error/utils/api-error-create.ts
@@ -5,13 +5,8 @@ import type { TApiErrorRaw } from '@/entities/api-error/types';
 import { ApiError } from './api-error';
 
 export const apiErrorCreate = (error: TApiErrorRaw | unknown): ApiError => {
-    let message: string | undefined = get(error, 'message');
-    let cause: string | undefined = get(error, 'cause');
-
-    if ([message, cause].includes(undefined)) {
-        message = 'Unexpected API error';
-        cause = 'api_error_unexpected';
-    }
+    const message: string = get(error, 'message') || 'Unexpected API error';
+    const cause: string = get(error, 'cause') || 'api_error';
 
     const apiError = new ApiError(message);
     apiError.cause = cause;
